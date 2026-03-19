92bcbbe2548576016ee64356e7a9a0b9f7b681d1
David Bernal González <dabernalgo@gmail.com>
2026-03-19 01:07:24 +0100

    Add TestPropertySource annotation to configure H2 database for test environments in product, order, and inventory service tests

--- microservices-platform/inventory-service/src/test/java/com/microservices/inventory/InventoryServiceApplicationTests.java
+++ microservices-platform/inventory-service/src/test/java/com/microservices/inventory/InventoryServiceApplicationTests.java
@@ -3,9 +3,17 @@ package com.microservices.inventory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+	"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=MySQL",
+	"spring.datasource.driver-class-name=org.h2.Driver",
+	"spring.datasource.username=sa",
+	"spring.datasource.password=",
+	"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class InventoryServiceApplicationTests {
 
 	@Test
--- microservices-platform/order-service/src/test/java/com/microservice/order/OrderServiceApplicationTests.java
+++ microservices-platform/order-service/src/test/java/com/microservice/order/OrderServiceApplicationTests.java
@@ -3,9 +3,17 @@ package com.microservice.order;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+	"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=MySQL",
+	"spring.datasource.driver-class-name=org.h2.Driver",
+	"spring.datasource.username=sa",
+	"spring.datasource.password=",
+	"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class OrderServiceApplicationTests {
 
 	@Test
